92a4a01151ea23516be3195b6737be1b4393b317
likun <likun7981@gmail.com>
2022-06-25 05:00:46 +0800

    feat: add ts support

---
+++ src/index.d.ts
@@ -0,0 +1,37 @@
+import { ListIterateeCustom, CollectionChain } from 'lodash';
+
+type Part<T> = Partial<T>
+
+type MergeId<T> = T & { id?: string }
+
+declare module "lodash" {
+  interface LoDashStatic extends TLodashId {}
+  interface CollectionChain<T> {
+    getById(id: string): CollectionChain<T>;
+    createId(): string;
+    upsert(doc: MergeId<T>): CollectionChain<T>;
+    insert(doc: T): CollectionChain<T>;
+    updateById(id: string, attrs: Part<T>): CollectionChain<T>;
+    updateWhere(predicate: ListIterateeCustom<T, boolean>, attrs: Part<T>): CollectionChain<T[]>;
+    replaceById(id: string, attrs: Part<T>): CollectionChain<T>;
+    removeById(id: string): CollectionChain<T>;
+    removeWhere(predicate: ListIterateeCustom<T, boolean>): CollectionChain<T[]>;
+  }
+}
+
+type TLodashId = {
+  id: string;
+  getById: <T>(array: Array<T>, id: string) => CollectionChain<T>,
+  createId: () => string,
+  insert: <T>(array: Array<T>, doc: T) => CollectionChain<T>,
+  updateById: <T>(array: Array<T>, id: string, attrs: Part<T>) => CollectionChain<T>,
+  updateWhere: <T>(array: Array<T>, predicate: ListIterateeCustom<T, boolean>, attrs: Part<T>) => CollectionChain<T[]>
+  upsert: <T>(array: Array<T>, doc: MergeId<T>) => CollectionChain<T>,
+  removeById: <T>(array: Array<T>, id: string, attrs: Part<T>) => CollectionChain<T>,
+  replaceById: <T>(array: Array<T>, id: string) => CollectionChain<T>,
+  removeWhere: <T>(array: Array<T>, predicate: ListIterateeCustom<T, boolean>) => CollectionChain<T[]>,
+}
+
+declare const lodashId: TLodashId
+
+export default lodashId
